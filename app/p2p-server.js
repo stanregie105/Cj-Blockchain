5281238a3b0df164279a866df72cdbc4063a677e
stanregie105 <stanleyregal004@yahoo.com>
2020-10-19 06:41:00 +0800

    modified app to include peer to peer socket connection and also enabled socket to send blockchain data through its send method to enable data sharing within peer to peer server

---
+++ app/p2p-server.js
@@ -0,0 +1,45 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+
+//$ HTTP_PORT =3002 P2P_PORT=5003 PEERS= ws://localhost:5001, ws://localhost:5002 npm run dev
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+    constructor(blockchain){
+        this.blockchain = blockchain;//this makes the p2pserver share its individual chain object with each other
+        this.sockets = [];//contains alist of the connected ws services connected to the server
+
+    }
+
+    listen(){
+      const server= new Websocket.Server({port:P2P_PORT});
+      server.on('connection', socket=> this.connectSocket(socket));
+      this.connectToPeers();
+      console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+    }// starts up the server
+
+    connectToPeers(){
+        peers.forEach(peer=>{
+            //ws://localhost:5001
+            const socket = new Websocket(peer);
+            socket.on('open',()=> this.connectSocket(socket));
+        })
+    }
+
+    connectSocket(socket){
+        this.sockets.push(socket);
+        console.log('socket connected');
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket){
+      socket.on('message', message=>{
+          const data = JSON.parse(message);
+          console.log('data',data);
+      })
+    }
+}
+
+module.exports= P2pServer;
